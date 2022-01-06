e1faff1e2caea66a8ada902527f11aea4358d5cf
GuyArieli17 <guyarieli17@gmail.com>
2022-01-06 19:10:40 +0000

    create training loop and evulation

--- run_controller.py
+++ run_controller.py
@@ -11,9 +11,106 @@ from tqdm import tqdm
 import pandas as pd
 import time
 
-def build_env():
-    # Todo: Implement
+class CityFlowEnv:
+    #Todo: implement
     pass
+
+
+def build_env(config,cityflow_config,model_dir,result_dir,
+                num_step,episodes,agent,phase_list,interval,phase_step,save_freq,
+                update_target_model_freq,learning_start,update_model_freq,algo):
+    # build cityflow environment
+    cityflow_config["saveReplay"] = False
+    json.dump(cityflow_config, open(config["cityflow_config_file"], 'w'))
+    # create env
+    env = CityFlowEnv(config)
+    # crete new folder if not exist
+    if not os.path.exists("model"):
+        os.makedirs("model")
+    if not os.path.exists("result"):
+        os.makedirs("result")
+    # create model | result folder
+    os.makedirs(model_dir)
+    os.makedirs(result_dir)
+
+    # training
+    total_step = 0
+    episode_rewards = []
+    episode_scores = []
+    # show progress in show way
+    with tqdm(total=episodes * num_step) as progress_bar:
+        for i in range(episodes):
+            # reset before each episodes
+            env.reset()
+            state = env.get_state()
+            episode_length = 0
+            episode_reward = 0
+            episode_score = 0
+            # run untill rech max number of steps
+            while episode_length < num_step:
+                # get action from agent
+                action = agent.choose_action(state)
+                # get the phase index of action
+                action_phase = phase_list[action]
+                # min action time (in number of states)
+                for _ in range(interval):
+                    next_state, reward = env.step(action_phase)  # one step
+                # update 
+                episode_length += 1
+                total_step += 1
+                episode_reward += reward
+                episode_score += env.get_score()
+
+                for _ in range(phase_step - 1):
+                    next_state, reward_ = env.step(action_phase)
+                    reward += reward_
+
+                reward /= phase_step
+
+                progress_bar.update(1)
+                # store to replay buffer
+                agent.remember(state, action_phase, reward, next_state)
+
+                state = next_state
+
+                # training
+                if total_step > learning_start and total_step % update_model_freq == 0:
+                    agent.replay()
+
+                # update target Q netwark
+                if total_step > learning_start and total_step % update_target_model_freq == 0:
+                    agent.update_target_network()
+
+                progress_bar.set_description(
+                    "total_step:{}, episode:{}, episode_step:{}, reward:{}".format(total_step, i + 1,
+                                                                                    episode_length, reward))
+
+
+            # save episode rewards
+            episode_rewards.append(episode_reward)
+            episode_scores.append(episode_score)
+            print("score: {}, mean reward:{}".format(episode_score, episode_reward / num_step))
+
+            # save model
+            if (i + 1) % save_freq == 0:
+                #Todo: save model
+
+                # save reward to file
+                df = pd.DataFrame({"rewards": episode_rewards})
+                df.to_csv(result_dir + '/rewards.csv', index=None)
+
+                df = pd.DataFrame({"rewards": episode_scores})
+                df.to_csv(result_dir + '/scores.csv', index=None)
+
+        # save figure
+        plot_data_lists([episode_rewards], ['episode reward'], figure_name=result_dir + '/rewards.pdf')
+        plot_data_lists([episode_scores], ['episode score'], figure_name=result_dir + '/scores.pdf')
+
+
+
+
+
+
 
 def interface():
     # Todo: Implement
@@ -55,6 +152,9 @@ def run():
     config_path = args.config
     num_step = args.num_step
     algo_str = args.algo
+    phase_step = args.phase_step
+    save_freq = args.save_freq
+    
 
 
     # get config file path
